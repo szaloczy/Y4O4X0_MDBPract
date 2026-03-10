3cec160d11db1acd4fd801a391ac9d0f47b81f86
Szalóczy Krisztián <krisztian.szaloczy@student.uni-miskolc.hu>
2026-03-10 14:13:08 +0100

    finish own API

--- Y4O4X0_0304/Y4O4X0DOMParse/src/y4o4x0/Y4O4X0DomReadOwn.java
+++ Y4O4X0_0304/Y4O4X0DOMParse/src/y4o4x0/Y4O4X0DomReadOwn.java
@@ -27,6 +27,10 @@ public class Y4O4X0DomReadOwn {
 
         printCeg(doc);
         printGyakornok(doc);
+        printTulajdonos(doc);
+        printMegrendelo(doc);
+        printFejleszto(doc);
+        printProjekt(doc);
     }
 
     private static void printCeg(Document doc) {
@@ -56,9 +60,7 @@ public class Y4O4X0DomReadOwn {
 
                 System.out.println("Id: " + id);
                 System.out.println("Név: " + name);
-                System.out.println("Város: " + city);
-                System.out.println("Utca: " + street);
-                System.out.println("Házszám: " + number);
+                System.out.println("Cím: " + city + " " + street + " " + number);
                 
             }
 
@@ -95,7 +97,92 @@ public class Y4O4X0DomReadOwn {
     }
 
     private static void printTulajdonos(Document doc) {
+        NodeList nList = doc.getElementsByTagName("tulajdonos");
+
+        for(int i = 0; i < nList.getLength(); i++) {
+            Node nNode = nList.item(i);
+
+            System.out.println("\nCurrent element: " + nNode.getNodeName());
+
+            if(nNode.getNodeType() == Node.ELEMENT_NODE) {
+                Element element = (Element) nNode;
+
+                String id = element.getAttribute("tkod");
+
+                System.out.println("Id: " + id);
+                System.out.println("Név: " + element.getElementsByTagName("nev").item(0).getTextContent());
+                System.out.println("Életkor: " + element.getElementsByTagName("eletkor").item(0).getTextContent());
+                
+                NodeList nList2 = element.getElementsByTagName("vegzettseg");
+                getMultipleValues(nList2, "Végzettség");
+            }
+        }
         
+    }
+
+    private static void printMegrendelo(Document doc) {
+        NodeList nList = doc.getElementsByTagName("megrendelo");
+
+        for(int i = 0; i < nList.getLength(); i++) {
+            Node nNode = nList.item(i);
+            
+            System.out.println("\nCurrent element: " + nNode.getNodeName());
+
+            if(nNode.getNodeType() == Node.ELEMENT_NODE) {
+                Element element = (Element) nNode;
+
+                System.out.println("Id: " + element.getAttribute("mkod"));
+                System.out.println("Név: " + element.getElementsByTagName("nev").item(0).getTextContent());
+                System.out.println("Határidő: " + element.getElementsByTagName("hatarido").item(0).getTextContent());
+                System.out.println("Cím: " + 
+                    element.getElementsByTagName("varos").item(0).getTextContent() + " " + 
+                    element.getElementsByTagName("utca").item(0).getTextContent() + " " + 
+                    element.getElementsByTagName("hazszam").item(0).getTextContent()
+                );
+            }
+        }
+    }
+
+    private static void printFejleszto(Document doc) {
+        NodeList nList = doc.getElementsByTagName("fejleszto");
+
+        for(int i = 0; i < nList.getLength(); i++) {
+            Node nNode = nList.item(i);
+            
+            System.out.println("\nCurrent element: " + nNode.getNodeName());
+
+            if(nNode.getNodeType() == Node.ELEMENT_NODE) {
+                Element element = (Element) nNode;
+
+                System.out.println("Id: " + element.getAttribute("mkod"));
+                System.out.println("Név: " + element.getElementsByTagName("nev").item(0).getTextContent());
+                System.out.println("Részleg: " + element.getElementsByTagName("reszleg").item(0).getTextContent());
+                System.out.println("Fizetes: " + element.getElementsByTagName("fizetes").item(0).getTextContent());
+                
+                NodeList nList2 = element.getElementsByTagName("vegzettseg");
+                getMultipleValues(nList2, "Végzettség");
+            }
+        }
+    }
+    
+    private static void printProjekt(Document doc) {
+        NodeList nList = doc.getElementsByTagName("projekt");
+
+        for(int i = 0; i < nList.getLength(); i++) {
+            Node nNode = nList.item(i);
+            
+            System.out.println("\nCurrent element: " + nNode.getNodeName());
+
+            if(nNode.getNodeType() == Node.ELEMENT_NODE) {
+                Element element = (Element) nNode;
+
+                System.out.println("Id: " + element.getAttribute("c_v_p"));
+                System.out.println("Név: " + element.getElementsByTagName("nev").item(0).getTextContent());
+                System.out.println("Összeg: " + element.getElementsByTagName("osszeg").item(0).getTextContent());
+                System.out.println("Hatarido: " + element.getElementsByTagName("hatarido").item(0).getTextContent());
+                System.out.println("Prioritas: " + element.getElementsByTagName("prioritas").item(0).getTextContent());
+            }
+        }
     }
 
     private static void getMultipleValues(NodeList nList, String tagName) {
