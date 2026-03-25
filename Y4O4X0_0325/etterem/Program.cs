f74168dd0ec9ca413f8d73e45c2295579d357dc6
szaloczy <szaloczy6@gmail.com>
2026-03-25 11:02:57 +0100

    finisg etterem

--- Y4O4X0_0325/etterem/Program.cs
+++ Y4O4X0_0325/etterem/Program.cs
@@ -12,10 +12,14 @@ namespace LinqApp
 
             //Console.WriteLine("(0. ) Full document: \n\n" + root);
 
-            FiveStartRestaurants(root);
+            FiveStarRestaurants(root);
+            ComplexQuery(root);
+            Aggregation1(root);
+            ModifyDocument(root);
+            Delete(root);
         }
 
-        static void FiveStartRestaurants(XElement root)
+        static void FiveStarRestaurants(XElement root)
         {
             Console.WriteLine("(1.) Five start restaurants: \n");
             var fiveStarRestaurants = root.Descendants("etterem")
@@ -26,5 +30,94 @@ namespace LinqApp
                 Console.WriteLine(" - " + e.Descendants("nev").First().Value)
             );
         }
+
+        static void ComplexQuery(XElement root)
+        {
+            Console.WriteLine("(2. ) Guest details: \n");
+
+            var triplexJoin = root.Descendants("rendeles")
+                .Select(e =>
+                {
+                    var guestID = e.Attribute("e_v_v")?.Value;
+
+                    var guestName = root.Descendants("vendeg")
+                        .Where(v => v.Attribute("vkod")?.Value == guestID)
+                        .FirstOrDefault()?
+                        .Descendants("nev")
+                        .FirstOrDefault()?
+                        .Value;
+
+                    var restaurantID = e.Attribute("e_v_e")?.Value;
+
+                    var restaurantName = root.Descendants("etterem")
+                        .Where(r => r.Attribute("ekod")?.Value == restaurantID)
+                        .FirstOrDefault()?
+                        .Descendants("nev")
+                        .FirstOrDefault()?
+                        .Value;
+
+                    var order = e.Descendants("etel").FirstOrDefault()?.Value;
+                    var sum = e.Descendants("osszeg").FirstOrDefault()?.Value;
+
+                    return new
+                    {
+                        Guest = guestName,
+                        Restaurant = restaurantName,
+                        Food = order,
+                        Sum = sum
+                    };
+                })
+                .ToList();
+
+            triplexJoin.ForEach(item =>
+                Console.WriteLine($"{item.Guest} - {item.Restaurant} - {item.Food} - {item.Sum}")
+            );
+        }
+
+        static void Aggregation1(XElement root)
+        {
+            var avgSpend =  root.Descendants("rendeles")
+                .Select(order => order.Descendants("osszeg").First().Value)
+                .Average(osszeg => double.Parse(osszeg));
+
+            System.Console.WriteLine($"(3. ) Average spending: {avgSpend}");
+        }
+
+        static void ModifyDocument(XElement root)
+        {
+            System.Console.WriteLine("(4. ) Doubling all order and saving in a new file \n");
+
+            root.Descendants("rendeles")
+                .ToList()
+                .ForEach(order =>
+                {
+                    var sumElement = order.Descendants("osszeg").First();
+                    var sum = double.Parse(sumElement.Value);
+
+                    sum *= 2;
+
+                    sumElement.Value = sum.ToString();
+                });
+
+                XDocument modifiedDocument = new XDocument(root);
+                modifiedDocument.Save("modified_etterem.xml");
+        }
+    
+        static void Delete(XElement root)
+        {
+            System.Console.WriteLine("(5. ) Delete all the 3 star restaurent: \n");
+
+            root.Descendants("etterem")
+                .Where(e => e.Descendants("csillag").First().Value == "3")
+                .ToList()
+                .ForEach(e =>
+                {
+                    e.Remove();
+                });
+
+                XDocument deletedeDocuemnt = new XDocument(root);
+                deletedeDocuemnt.Save("deleted_etterem.xml");
+        }
+    
     }
 }
---
+++ Y4O4X0_0325/own/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace OwnLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument document = XDocument.Load("./xml/Y4O4X0XMLOwn.xml");
+            XElement root = document.Descendants("cegek").First();
+
+            //Console.WriteLine("(0. ) Full document: \n\n" + root);
+
+            internsWith12week(root);
+        }
+
+        static void internsWith12week(Xelement root)
+        {
+            
+        }
+    }
+}
