43946ded6811c649539d9ae15affa458b991dd36
szaloczy <szaloczy6@gmail.com>
2026-03-25 09:00:08 +0100

    Implement Task 2

---
+++ Y4O4X0_0325/etterem/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace LinqApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument document = XDocument.Load("./xml/etterem.xml");
+            XElement root = document.Descendants("vendeglatas").First();
+
+            //Console.WriteLine("(0. ) Full document: \n\n" + root);
+
+            FiveStartRestaurants(root);
+        }
+
+        static void FiveStartRestaurants(XElement root)
+        {
+            Console.WriteLine("(1.) Five start restaurants: \n");
+            var fiveStarRestaurants = root.Descendants("etterem")
+                .Where(e => e.Descendants("csillag").First().Value == "5")
+                .ToList();
+
+            fiveStarRestaurants.ForEach(e => 
+                Console.WriteLine(" - " + e.Descendants("nev").First().Value)
+            );
+        }
+    }
+}
